cdb41d885bde961ce5ec9e011ef8c6be6bb43b72
Aastha <aasthaktayde@gmail.com>
2026-07-02 19:19:21 +0530

    Added User's model,authcontroller,authroutes,server,js and implemented authentication

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth",authRoutes);
 
-app.use("/api/auth", authRoutes);
-
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log(err);
-});
-
-
-app.get("/", (req,res)=>{
-    res.send("ShelfShare Backend Running");
-});
-
-
-app.listen(5000,()=>{
-    console.log("Server running on port 5000");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
