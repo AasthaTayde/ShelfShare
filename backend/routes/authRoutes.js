d3f97910b807bd9091c8e2352eb1647d5a648278
Aastha <aasthaktayde@gmail.com>
2026-07-02 19:30:03 +0530

    Added JWT authentication middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/authController");
-
+const auth = require("../middleware/midauth");
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/profile", auth, (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Protected route accessed",
+      user: req.user,
+    });
+  });
+  
 
 module.exports = router;
